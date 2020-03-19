e87b9284e3899870edd4cd98df499699d27a4ba0
Wayne <wayne.folkes@gmail.com>
2020-03-19 15:12:54 -0400

    explicitly output json when listing policies

--- apply_scp.sh
+++ apply_scp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Applying Security Controls SCP to all accounts"
-policy_id=`aws organizations list-policies --filter SERVICE_CONTROL_POLICY | jq --arg p "$policy" -c '.Policies[] | select( .Name | contains("SecurityControls"))' | jq -r .Id `
+policy_id=`aws organizations list-policies --filter SERVICE_CONTROL_POLICY --output json | jq --arg p "$policy" -c '.Policies[] | select( .Name | contains("SecurityControls"))' | jq -r .Id `
 for account_id in `aws organizations list-accounts | jq -r .Accounts[].Id` ; do
     aws organizations attach-policy --policy-id $policy_id --target-id $account_id --output text
 done
