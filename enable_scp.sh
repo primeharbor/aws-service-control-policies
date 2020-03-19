883df1187f611aefe5c36064e20a9e1945accabd
Wayne <wayne.folkes@gmail.com>
2020-03-19 15:26:33 -0400

    make sure list-roots outputs in json

--- enable_scp.sh
+++ enable_scp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Enabling SCPs for this account"
-root_id=`aws organizations list-roots | jq -r .Roots[].Id`
+root_id=`aws organizations list-roots --output json | jq -r .Roots[].Id`
 
 aws organizations enable-policy-type --root-id $root_id --policy-type SERVICE_CONTROL_POLICY --output text
 
