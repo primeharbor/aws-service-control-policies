0cca003f22fff820b3f03758e95b3eccb907653c
Chris Farris <chris@room17.com>
2022-06-05 06:51:55 -0400

    Cleanup old stuff

--- enable_scp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Enabling SCPs for this account"
-root_id=`aws organizations list-roots | jq -r .Roots[].Id`
-
-aws organizations enable-policy-type --root-id $root_id --policy-type SERVICE_CONTROL_POLICY --output text
-
-exit 0
